89da58799f9c0756e75e1970fd82d32bb0969033
ktoonsez <ktoonsez@gmail.com>
2012-06-06 10:03:37 -0700

    update cm to aokp copy

--- cp_from_cm_to_aokp.sh
+++ cp_from_cm_to_aokp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mkdir ../aokp/kernel/samsung/smdk4210/arch/arm/configs
+mkdir ../aokp/kernel/samsung/smdk4210
 cp ../android/system/kernel/samsung/smdk4210/* ../aokp/kernel/samsung/smdk4210
 
