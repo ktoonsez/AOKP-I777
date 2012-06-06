f61070d01d1899efd2e6d658a61daee0c3ba63e2
ktoonsez <ktoonsez@gmail.com>
2012-06-06 10:25:55 -0700

    copy from cm to aokp script update

--- cp_from_cm_to_aokp.sh
+++ cp_from_cm_to_aokp.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-mkdir ../aokp/kernel/samsung/smdk4210
+if [ ! -d ../aokp/kernel/samsung/smdk4210 ];
+then
+   echo "Make directory aokp/kernel/samsung/smdk4210"
+   mkdir ../aokp/kernel/samsung/smdk4210
+fi
+echo "Copy directory android/system/kernel/samsung/smdk4210"
 cp ../android/system/kernel/samsung/smdk4210/* ../aokp/kernel/samsung/smdk4210
 
