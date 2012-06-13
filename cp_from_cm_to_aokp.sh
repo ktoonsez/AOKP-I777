03eee6e468542c8ad62e88d2b0346e4010cd2063
ktoonsez <ktoonsez@gmail.com>
2012-06-12 21:31:15 -0700

    updated cp_from_cm_to_aokp.sh

--- cp_from_cm_to_aokp.sh
+++ cp_from_cm_to_aokp.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+echo "Remove old aokp/kernel/samsung/smdk4210"
+rm -R ../aokp/kernel/samsung/smdk4210
+
 if [ ! -d ../aokp/kernel/samsung/smdk4210 ];
 then
    echo "Make directory aokp/kernel/samsung/smdk4210"
    mkdir ../aokp/kernel/samsung/smdk4210
 fi
 echo "Copy directory android/system/kernel/samsung/smdk4210"
-cp ../android/system/kernel/samsung/smdk4210/* ../aokp/kernel/samsung/smdk4210
+cp -R ../android/system/kernel/samsung/smdk4210/* ../aokp/kernel/samsung/smdk4210
 
