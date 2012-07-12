3491ddf77798f12b8253fb2b5ea7baab00706729
ktoonsez <ktoonsez@gmail.com>
2012-07-11 22:29:25 -0700

    updated list for I747

--- vendor/aokp/products/AndroidProducts.mk
+++ vendor/aokp/products/AndroidProducts.mk
@@ -13,6 +13,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/inc.mk \
     $(LOCAL_DIR)/mesmerizemtd.mk \
     $(LOCAL_DIR)/n7000.mk \
+    $(LOCAL_DIR)/d2att.mk \
     $(LOCAL_DIR)/otter.mk \
     $(LOCAL_DIR)/p1c.mk \
     $(LOCAL_DIR)/p4.mk \
